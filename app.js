6836d7fb032d3366592d9638b5c49e5e1a207dc6
pun2pun <35132090+pun2pun@users.noreply.github.com>
2020-10-15 23:23:40 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,58 +1,17 @@
-/*
+
 var express = require('express');
 var app = express();
 var request = require('request');
 var ntpClient = require('ntp-client');
 
-app.get('/', function(req, res, next) {
-  var message =  "ทดสอบ" //req.body.message;
+app.set ( "view engine", "ejs" );
 
-  request({
-    method: 'POST',
-    uri: 'https://notify-api.line.me/api/notify',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    auth: {
-      'bearer': "k1kc1HFm01mwwFy8wTgG3YnJc0ztsnIQDJZfwrknr4i"
-    },
-    form: {
-      message: message
-    }
-  }, (err, httpResponse, body) => { res.send(200) });
+app.get ( "/", function ( req, res ) 
+	{
+		res.render ( "homepage" );		
+	}
+)
 
+app.listen (80,function(){
+	console.log("Server runing...")
 });
-
-app.listen(8080);
-
-*/
-
-var ntpClient = require('ntp-client');
-var send_status = true;
-
-while(1){
-	setTimeout(function(){
-		ntpClient.getNetworkTime("clock.nectec.or.th",123, function(err, date) {
-			console.log(date)
-			var message =  "ทดสอบส่งงเวลา"
-				if(date.getHours() == 14 && date.getMinutes() == 5 && send_status == true){
-					request({
-						method: 'POST',
-						uri: 'https://notify-api.line.me/api/notify',
-						headers: {
-						  'Content-Type': 'application/x-www-form-urlencoded'
-						},
-						auth: {
-						  'bearer': "k1kc1HFm01mwwFy8wTgG3YnJc0ztsnIQDJZfwrknr4i"
-						},
-						form: {
-						  message: message
-						}
-					  }, (err, httpResponse, body) => { res.send(200) });		 
-					  send_status = false 	  
-				}
-			});
-	},1000)
-
-
-}
